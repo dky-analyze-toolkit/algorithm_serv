9ca4fcbf8fa03f695df54af5b81cfb7bc321f945
lvjz2018 <lvjianzhang@sina.com>
2020-12-31 09:23:42 +0800

    SM2 SM3 SM4

--- src/main/java/com/toolkit/algorithm_serv/controller/SMController.java
+++ src/main/java/com/toolkit/algorithm_serv/controller/SMController.java
@@ -2,7 +2,14 @@ package com.toolkit.algorithm_serv.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import com.toolkit.algorithm_serv.global.response.ResponseHelper;
-import com.toolkit.algorithm_serv.global.utils.SystemUtils;
+import com.toolkit.algorithm_serv.global.utils.SecurityTestAll;
+import com.toolkit.algorithm_serv.global.utils.Util;
+import com.toolkit.algorithm_serv.global.utils.sm2.SM2EncDecUtils;
+import com.toolkit.algorithm_serv.global.utils.sm2.SM2SignVO;
+import com.toolkit.algorithm_serv.global.utils.sm2.SM2SignVerUtils;
+import com.toolkit.algorithm_serv.global.utils.sm4.SM4Utils;
+import org.bouncycastle.crypto.digests.SM3Digest;
+import org.bouncycastle.util.encoders.Hex;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,46 +27,201 @@ public class SMController {
     ResponseHelper responseHelper;
 
     /**
-     * A.1 获取操作系统信息
+     * 1.1 SM2签名
+     * publicKey = "042780f0963a428a7b030ac1c14a90b967bf365f5394ebf1f0ca1598d4d9bece4fdfa05ba043817fef68bef497088e3992362ce55b1858444fa5a3e00c5042b207";
+     * privatekey = "73e83d33d95274eeeb23f01834d02fe920b4afece377410435698dfdf1d84203";
+     * src = "0653F3748DFD938FE83935800FF3F526B85C30C2331DD56FCB1794AA99F2A416";
      * @return
      */
     @RequestMapping(value = "/sm2sign", method = RequestMethod.GET)
     @ResponseBody
-    public Object getOsInfo() {
+    public Object sm2sign(@RequestParam("privatekey") String privatekey,
+                          @RequestParam("srchex") String srchex) throws Exception {
+
+        SM2SignVO sign = SM2SignVerUtils.Sign2SM2(Util.hexStringToBytes(privatekey), Util.hexToByte(srchex));
+        System.out.println("R:"+sign.sign_r);
+        System.out.println("S:"+sign.sign_s);
+
         JSONObject jsonOS = new JSONObject();
-        jsonOS.put("OS Name", SystemUtils.getOsName());
-        jsonOS.put("OS Arch", SystemUtils.getOsArch());
-        jsonOS.put("OS Version", SystemUtils.getOsVersion());
+        jsonOS.put("sign_r", sign.sign_r);
+        jsonOS.put("sign_s", sign.sign_s);
         return responseHelper.success(jsonOS);
     }
 
     /**
-     * A.2 获取所有系统环境参数
+     * 1.2 SM2验签
+     * publicKey = "042780f0963a428a7b030ac1c14a90b967bf365f5394ebf1f0ca1598d4d9bece4fdfa05ba043817fef68bef497088e3992362ce55b1858444fa5a3e00c5042b207";
+     * privatekey = "73e83d33d95274eeeb23f01834d02fe920b4afece377410435698dfdf1d84203";
+     * src = "0653F3748DFD938FE83935800FF3F526B85C30C2331DD56FCB1794AA99F2A416";
      * @return
      */
-    @RequestMapping(value = "/all-sys-props", method = RequestMethod.GET)
+    @RequestMapping(value = "/sm2verify", method = RequestMethod.GET)
     @ResponseBody
-    public Object getAllSystemProps() {
-        return responseHelper.success(SystemUtils.sysProps);
+    public Object sm2verify(@RequestParam("publickey") String publickey,
+                            @RequestParam("srchex") String srchex,
+                            @RequestParam("signhex") String signhex  ) throws Exception {
+
+        SM2SignVO verify = SM2SignVerUtils.VerifySignSM2(Util.hexStringToBytes(publickey), Util.hexToByte(srchex), Util.hexToByte(SecurityTestAll.SM2SignHardToSoft(signhex)));
+        System.err.println("验签结果" + verify.isVerify());
+
+        JSONObject jsonOS = new JSONObject();
+        jsonOS.put("验签结果", verify.isVerify());
+        return responseHelper.success(jsonOS);
+    }
+    /**
+     * 1.3 SM2加密
+     * publicKey = "042780f0963a428a7b030ac1c14a90b967bf365f5394ebf1f0ca1598d4d9bece4fdfa05ba043817fef68bef497088e3992362ce55b1858444fa5a3e00c5042b207";
+     * privatekey = "73e83d33d95274eeeb23f01834d02fe920b4afece377410435698dfdf1d84203";
+     * src = "0653F3748DFD938FE83935800FF3F526B85C30C2331DD56FCB1794AA99F2A416";
+     * @return
+     */
+    @RequestMapping(value = "/sm2enc", method = RequestMethod.GET)
+    @ResponseBody
+    public Object sm2enc(@RequestParam("publickey") String publicKey,
+                        @RequestParam("plainhex") String src) throws Exception {
+
+//        String plainText = "ILoveYou11";
+//        byte[] sourceData1 = plainText.getBytes();
+        byte[] sourceData = Util.hexToByte(src);
+
+        System.out.println("加密: ");
+        String cipherText = SM2EncDecUtils.encrypt(Util.hexToByte(publicKey), sourceData);
+        System.out.println(cipherText);
+        JSONObject jsonOS = new JSONObject();
+        jsonOS.put("cipherText", cipherText);
+        return responseHelper.success(jsonOS);
+
     }
 
-//    /**
-//     * A.3 读取全局变量
-//     * @param varType 全局变量类型
-//     * @return
-//     */
-//    @RequestMapping(value = "/global-vars", method = RequestMethod.GET)
-//    @ResponseBody
-//    public Object getGlobalVars(@RequestParam("var_type") String varType) {
-//        if (varType.equalsIgnoreCase("host")) {
-//            JSONObject jsonResp = new JSONObject();
-//            jsonResp.put("host_ip", HostConfigs.ip);
-//            jsonResp.put("host_port", HostConfigs.port);
-//            jsonResp.put("host_url_alive", HostConfigs.urlTestAlive);
-//            return responseHelper.success(jsonResp);
-//        }
-//
-//        return responseHelper.error(ErrorCodeEnum.ERROR_PARAMETER);
-//    }
+    /**
+     * 1.4 SM2解密
+     * publicKey = "042780f0963a428a7b030ac1c14a90b967bf365f5394ebf1f0ca1598d4d9bece4fdfa05ba043817fef68bef497088e3992362ce55b1858444fa5a3e00c5042b207";
+     * privatekey = "73e83d33d95274eeeb23f01834d02fe920b4afece377410435698dfdf1d84203";
+     * src = "0653F3748DFD938FE83935800FF3F526B85C30C2331DD56FCB1794AA99F2A416";
+     * @return
+     */
+    @RequestMapping(value = "/sm2dec", method = RequestMethod.GET)
+    @ResponseBody
+    public Object sm2dec(@RequestParam("privatekey") String privatekey,
+                       @RequestParam("cipherhex") String cipherhex) throws Exception {
+
+        System.out.println("解密: ");
+        String plainText = new String(SM2EncDecUtils.decrypt(Util.hexToByte(privatekey), Util.hexToByte(cipherhex)));
+        System.out.println(plainText);
+
+        JSONObject jsonOS = new JSONObject();
+        jsonOS.put("plainText", plainText);
+        return responseHelper.success(jsonOS);
+    }
+
+    /**
+     * 1.5 SM3Digest
+     * publicKey = "042780f0963a428a7b030ac1c14a90b967bf365f5394ebf1f0ca1598d4d9bece4fdfa05ba043817fef68bef497088e3992362ce55b1858444fa5a3e00c5042b207";
+     * privatekey = "73e83d33d95274eeeb23f01834d02fe920b4afece377410435698dfdf1d84203";
+     * src = "0653F3748DFD938FE83935800FF3F526B85C30C2331DD56FCB1794AA99F2A416";
+     * @return
+     */
+    @RequestMapping(value = "/sm3digest", method = RequestMethod.GET)
+    @ResponseBody
+    public Object sm3(@RequestParam("srchex") String srchex) throws Exception {
+        byte[] md = new byte[32];
+        byte[] msg1 = Util.hexToByte(srchex);
+//        byte[] msg1 = "ererfeiisgod".getBytes();
+        System.out.println(Util.byteToHex(msg1));
+        SM3Digest sm3 = new SM3Digest();
+        sm3.update(msg1, 0, msg1.length);
+        sm3.doFinal(md, 0);
+        String s = new String(Hex.encode(md));
+        System.out.println(s.toUpperCase());
+
+        JSONObject jsonOS = new JSONObject();
+        jsonOS.put("SM3Digest", s.toUpperCase());
+        return responseHelper.success(jsonOS);
+    }
+
+    /**
+     * 1.6 SM4加密
+     * publicKey = "042780f0963a428a7b030ac1c14a90b967bf365f5394ebf1f0ca1598d4d9bece4fdfa05ba043817fef68bef497088e3992362ce55b1858444fa5a3e00c5042b207";
+     * privatekey = "73e83d33d95274eeeb23f01834d02fe920b4afece377410435698dfdf1d84203";
+     * src = "0653F3748DFD938FE83935800FF3F526B85C30C2331DD56FCB1794AA99F2A416";
+     * @return
+     */
+    @RequestMapping(value = "/sm4enc", method = RequestMethod.GET)
+    @ResponseBody
+    public Object sm4enc(@RequestParam("key") String key,
+                         @RequestParam("plainhex") String plainhex,
+                         @RequestParam("mode") String mode) throws Exception {
+        byte[] sourceData = Util.hexToByte(plainhex);
+
+        SM4Utils sm4 = new SM4Utils();
+        sm4.secretKey = key;
+        sm4.hexString = true;
+        String cipherText="";
+        if(mode.equals("ECB"))
+        {
+            System.out.println("ECB模式加密");
+            cipherText = sm4.encryptData_ECB_hex(sourceData);
+            System.out.println("密文: " + cipherText);
+            System.out.println("");
+
+        }
+        else if(mode.equals("CBC"))
+        {
+            System.out.println("CBC模式加密");
+            sm4.iv = "30303030303030303030303030303030";
+            cipherText = sm4.encryptData_CBC_hex(sourceData);
+            System.out.println("加密密文: " + cipherText);
+            System.out.println("");
+
+        }
+
+        JSONObject jsonOS = new JSONObject();
+        jsonOS.put("cipherText", cipherText);
+        return responseHelper.success(jsonOS);
+    }
+
+    /**
+     * 1.4 SM4解密
+     * publicKey = "042780f0963a428a7b030ac1c14a90b967bf365f5394ebf1f0ca1598d4d9bece4fdfa05ba043817fef68bef497088e3992362ce55b1858444fa5a3e00c5042b207";
+     * privatekey = "73e83d33d95274eeeb23f01834d02fe920b4afece377410435698dfdf1d84203";
+     * src = "0653F3748DFD938FE83935800FF3F526B85C30C2331DD56FCB1794AA99F2A416";
+     * @return
+     */
+    @RequestMapping(value = "/sm4dec", method = RequestMethod.GET)
+    @ResponseBody
+    public Object sm4dec(@RequestParam("key") String key,
+                         @RequestParam("cipherhex") String cipherhex,
+                         @RequestParam("mode") String mode) throws Exception {
+
+        byte[] sourceData = Util.hexToByte(cipherhex);
+
+        SM4Utils sm4 = new SM4Utils();
+        sm4.secretKey = key;
+        sm4.hexString = true;
+        sm4.iv = "30303030303030303030303030303030";
+        String plainText="";
+        if(mode.equals("ECB"))
+        {
+            System.out.println("ECB模式解密");
+            byte[] encData = Util.hexToByte(cipherhex);
+            plainText = sm4.decryptData_ECB_hex(encData);
+            System.out.println("明文: " + plainText);
+            System.out.println("");
+        }
+        else if(mode.equals("CBC"))
+        {
+            System.out.println("CBC模式解密");
+            byte[] encData = Util.hexToByte(cipherhex);
+            plainText = sm4.decryptData_CBC_hex(encData);
+            System.out.println("明文: " + plainText);
+            System.out.println("");
+
+        }
+
+        JSONObject jsonOS = new JSONObject();
+        jsonOS.put("plainText", plainText);
+        return responseHelper.success(jsonOS);
+    }
+
 
 }
--- src/main/java/com/toolkit/algorithm_serv/global/utils/sm4/SM4Utils.java
+++ src/main/java/com/toolkit/algorithm_serv/global/utils/sm4/SM4Utils.java
@@ -23,6 +23,125 @@ public class SM4Utils {
     public SM4Utils() {
     }
 
+    public String encryptData_ECB_hex(byte[] data) {
+        try {
+            SM4_Context ctx = new SM4_Context();
+            ctx.isPadding = true;
+            ctx.mode = SM4.SM4_ENCRYPT;
+
+            byte[] keyBytes;
+            if (hexString) {
+                keyBytes = Util.hexStringToBytes(secretKey);
+            } else {
+                //keyBytes = secretKey.getBytes();
+                keyBytes = Util.hexStringToBytes(secretKey);
+            }
+
+            SM4 sm4 = new SM4();
+            sm4.sm4_setkey_enc(ctx, keyBytes);
+            byte[] encrypted = sm4.sm4_crypt_ecb(ctx, data);
+            return Util.byteToHex(encrypted);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public String decryptData_ECB_hex(byte[] encrypted) {
+        try {
+//            byte[] encrypted = Util.hexToByte(cipherText);
+            String cipherText=Base64.encodeBase64String(encrypted);;
+            //cipherText = new BASE64Encoder().encode(encrypted);
+            if (cipherText != null && cipherText.trim().length() > 0) {
+                Pattern p = Pattern.compile("\\s*|\t|\r|\n");
+                Matcher m = p.matcher(cipherText);
+                cipherText = m.replaceAll("");
+            }
+
+            SM4_Context ctx = new SM4_Context();
+            ctx.isPadding = true;
+            ctx.mode = SM4.SM4_DECRYPT;
+
+            byte[] keyBytes;
+            if (hexString) {
+                keyBytes = Util.hexStringToBytes(secretKey);
+            } else {
+                keyBytes = secretKey.getBytes();
+            }
+
+            SM4 sm4 = new SM4();
+            sm4.sm4_setkey_dec(ctx, keyBytes);
+            byte[] decrypted = sm4.sm4_crypt_ecb(ctx, Base64.decodeBase64(cipherText));
+            //byte[] decrypted = sm4.sm4_crypt_ecb(ctx, new BASE64Decoder().decodeBuffer(cipherText));
+            return new String(decrypted, "UTF-8");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public String encryptData_CBC_hex(byte[] data) {
+        try {
+            SM4_Context ctx = new SM4_Context();
+            ctx.isPadding = true;
+            ctx.mode = SM4.SM4_ENCRYPT;
+
+            byte[] keyBytes;
+            byte[] ivBytes;
+            if (hexString) {
+                keyBytes = Util.hexStringToBytes(secretKey);
+                ivBytes = Util.hexStringToBytes(iv);
+            } else {
+                keyBytes = secretKey.getBytes();
+                ivBytes = iv.getBytes();
+            }
+
+            SM4 sm4 = new SM4();
+            sm4.sm4_setkey_enc(ctx, keyBytes);
+            byte[] encrypted = sm4.sm4_crypt_cbc(ctx, ivBytes, data);
+            return Util.byteToHex(encrypted);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public String decryptData_CBC_hex(byte[] encrypted) {
+        try {
+//            byte[] encrypted = Util.hexToByte(cipherText);
+            String cipherText=Base64.encodeBase64String(encrypted);;
+            //cipherText = new BASE64Encoder().encode(encrypted);
+            if (cipherText != null && cipherText.trim().length() > 0) {
+                Pattern p = Pattern.compile("\\s*|\t|\r|\n");
+                Matcher m = p.matcher(cipherText);
+                cipherText = m.replaceAll("");
+            }
+            SM4_Context ctx = new SM4_Context();
+            ctx.isPadding = true;
+            ctx.mode = SM4.SM4_DECRYPT;
+
+            byte[] keyBytes;
+            byte[] ivBytes;
+            if (hexString) {
+                keyBytes = Util.hexStringToBytes(secretKey);
+                ivBytes = Util.hexStringToBytes(iv);
+            } else {
+                keyBytes = secretKey.getBytes();
+                ivBytes = iv.getBytes();
+            }
+
+            SM4 sm4 = new SM4();
+            sm4.sm4_setkey_dec(ctx, keyBytes);
+            //byte[] decrypted = sm4.sm4_crypt_cbc(ctx, ivBytes, new BASE64Decoder().decodeBuffer(cipherText));
+            byte[] decrypted = sm4.sm4_crypt_cbc(ctx, ivBytes, Base64.decodeBase64(cipherText));
+            /*String text = new String(decrypted, "UTF-8");
+            return text.substring(0,text.length()-1);*/
+            return new String(decrypted, "UTF-8");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
     public String encryptData_ECB(String plainText) {
         try {
