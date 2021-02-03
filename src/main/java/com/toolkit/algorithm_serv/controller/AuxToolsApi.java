60731ef35a15a21df311076aff0b94f499358973
ytwei72 <249422@qq.com>
2021-02-03 15:19:04 +0800

    add transform interfaces

--- src/main/java/com/toolkit/algorithm_serv/controller/AuxToolsApi.java
+++ src/main/java/com/toolkit/algorithm_serv/controller/AuxToolsApi.java
@@ -1,17 +1,24 @@
 package com.toolkit.algorithm_serv.controller;
 
 import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.net.Ipv4Util;
+import cn.hutool.core.util.HexUtil;
 import cn.hutool.core.util.IdUtil;
+import cn.hutool.json.JSONUtil;
 import com.alibaba.fastjson.JSONObject;
 
+import com.google.common.base.Strings;
 import com.toolkit.algorithm_serv.annotation.SysAuth;
 import com.toolkit.algorithm_serv.global.enumeration.ErrorCodeEnum;
+import com.toolkit.algorithm_serv.global.exception.ExceptionHelper;
 import com.toolkit.algorithm_serv.global.response.ResponseHelper;
 import com.toolkit.algorithm_serv.utils.StrAuxUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.math.BigInteger;
 
 import static com.toolkit.algorithm_serv.algorithm.auxtools.RandomHelper.generateRandom;
 
@@ -21,10 +28,12 @@ public class AuxToolsApi {
     protected Logger logger = LoggerFactory.getLogger(this.getClass());
 
     private final ResponseHelper responseHelper;
+    private final ExceptionHelper exceptionHelper;
 
     @Autowired
-    public AuxToolsApi(ResponseHelper responseHelper) {
+    public AuxToolsApi(ResponseHelper responseHelper, ExceptionHelper exceptionHelper) {
         this.responseHelper = responseHelper;
+        this.exceptionHelper = exceptionHelper;
     }
 
     @SysAuth
@@ -82,4 +91,48 @@ public class AuxToolsApi {
         }
     }
 
+    @GetMapping("/base-convert")
+    @ResponseBody
+    public Object baseConvert(@RequestParam("src_number") String srcNumber,
+                              @RequestParam("src_radix") int srcRadix,
+                              @RequestParam("dest_radix") int destRadix) {
+        try {
+            JSONObject jsonResult = new JSONObject();
+            JSONObject jsonSrc = new JSONObject();
+            jsonSrc.put("number", srcNumber);
+            jsonSrc.put("radix", srcRadix);
+
+            JSONObject jsonDest = new JSONObject();
+            BigInteger src = new BigInteger(srcNumber, srcRadix);
+            jsonDest.put("number", src.toString(destRadix));
+            jsonDest.put("radix", destRadix);
+
+            jsonResult.put("src", jsonSrc);
+            jsonResult.put("dest", jsonDest);
+
+            return responseHelper.success(jsonResult);
+        } catch (Exception e) {
+            return exceptionHelper.response(e);
+        }
+    }
+
+    @GetMapping("/ip-convert")
+    @ResponseBody
+    public Object ipConvert(@RequestParam(value = "ip_str", required = false) String ipStr,
+                            @RequestParam(value = "ip_num", required = false) long ipNum) {
+        try {
+            JSONObject jsonResult = new JSONObject();
+            if (Strings.isNullOrEmpty(ipStr)) {
+                ipStr = Ipv4Util.longToIpv4(ipNum);
+            } else {
+                ipNum = Ipv4Util.ipv4ToLong(ipStr);
+            }
+            jsonResult.put("addr", ipStr);
+            jsonResult.put("number", ipNum);
+            jsonResult.put("hex", HexUtil.toHex(ipNum));
+            return responseHelper.success(jsonResult);
+        } catch (Exception e) {
+            return exceptionHelper.response(e);
+        }
+    }
 }
